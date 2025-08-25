b82face773cf697ad2b5f934b9872100d27cd2f2
MuhammadYasinSaleem <m.yaseensalim@gmail.com>
2025-08-25 15:35:24 +0500

    test: pre-commit

--- locations/admin.py
+++ locations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Location
 
+
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name']
-    search_fields = ['name']
+    list_display = ["id", "name"]
+    search_fields = ["name"]
--- locations/models.py
+++ locations/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Location(models.Model):
     name = models.CharField(max_length=100)
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
